8a4e2ba8fdd853d1c3663e79a59574ca76ef9a39
William He <supervegeter@gmail.com>
2017-02-16 23:40:47 +0800

    Added sample proxy config usage.

--- vue/config/index.js
+++ vue/config/index.js
@@ -27,7 +27,16 @@ module.exports = {
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      // proxy all requests starting with /api to jsonplaceholder
+      '*': {
+        target: 'http://jsonplaceholder.typicode.com',
+        changeOrigin: true,
+        filter: function(pathname, req) {
+          return pathname.match('^/api') && req.method === 'GET'
+        }
+      }
+    },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
