55c860c41b3a17286f2c7f6959c1953985f30f4e
Williammer <supervegeter@gmail.com>
2017-02-26 10:56:11 +0800

    move App.css

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import NavLink from './components/NavLink'
 import logo from './logo.svg'
-import './App.css';
+import './css/App.css';
 
 class App extends Component {
   render() {
