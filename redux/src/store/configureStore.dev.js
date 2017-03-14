161a74d784f57b7dd29214b1bf406b0cd724c6d0
Williammer <supervegeter@gmail.com>
2017-03-14 22:01:43 +0800

    Removed DevTool/Logger

--- redux/src/store/configureStore.dev.js
+++ redux/src/store/configureStore.dev.js
@@ -1,18 +1,13 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
-import createLogger from 'redux-logger'
 import api from '../middleware/api'
 import rootReducer from '../reducers'
-import DevTools from '../containers/DevTools'
 
 const configureStore = preloadedState => {
   const store = createStore(
     rootReducer,
     preloadedState,
-    compose(
-      applyMiddleware(thunk, api, createLogger()),
-      DevTools.instrument()
-    )
+    applyMiddleware(thunk, api)
   )
 
   if (module.hot) {
