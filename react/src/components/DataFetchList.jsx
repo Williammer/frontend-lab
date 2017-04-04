03bd2f4458af17ec323fc1138f4ca21d9f89309e
Williammer <supervegeter@gmail.com>
2017-04-04 19:26:41 +0800

    refactored react DataFetchList

--- react/src/components/DataFetchList.jsx
+++ react/src/components/DataFetchList.jsx
@@ -1,24 +1,109 @@
 import React, { PropTypes, Component } from 'react'
 import axios from 'axios'
 
+
+function NoResult() {
+  return (
+    <div>
+      "no result"
+    </div>
+  );
+}
+
+function renderDataToList(data) {
+  let result = [];
+  if (data && data.length > 0) {
+    result = data.map((repo) => <li key={repo.id}>{repo.name}</li>);
+  }
+
+  return result;
+}
+
+
+/**
+ * SearchBar Component
+ */
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.inputSearch = this.inputSearch.bind(this);
+  }
+
+  inputSearch(evt) {
+    const val = evt.target.value;
+    this.props.onUserInput(val);
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          name="searchbar"
+          type="text"
+          placeholder="Search..."
+          onChange={this.inputSearch}
+        />
+      </div>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  onUserInput: PropTypes.func.isRequired
+}
+
+
+/**
+ * RepoList Component
+ */
+class RepoList extends Component {
+  render() {
+    const { username, repos } = this.props;
+
+    return (
+      <div>
+        <h1>Repos of {username}:</h1>
+        <ul className="repo-list">{renderDataToList(repos)}</ul>
+      </div>
+    )
+  }
+}
+
+RepoList.propTypes = {
+  username: PropTypes.string.isRequired,
+  repos: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired
+  })).isRequired
+}
+
+
+/**
+ * DataFetchList Component
+ */
 class DataFetchList extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       repos: [],
-      isFetching: false
+      isFetching: false,
+      searchKeyword: ''
     };
+
+    this.handleUserInput = this.handleUserInput.bind(this);
+    this.getFilteredRepo = this.getFilteredRepo.bind(this);
   }
 
-  static propTypes = {
-    fetch: PropTypes.func,
-    username: PropTypes.string.isRequired,
+  handleUserInput(value) {
+    this.setState({
+      searchKeyword: value
+    });
   }
 
-  static defaultProps = {
-    fetch: axios.get,
-    username: 'Williammer'
+  getFilteredRepo() {
+    const { repos } = this.state;
+    return repos.filter((repo) => repo.name.includes(this.state.searchKeyword));
   }
 
   fetchRepos(username) {
@@ -50,10 +135,15 @@ class DataFetchList extends Component {
         {
           isFetching ? <span>fetching...</span> :
             (repos && repos.length > 0) ?
-              <SearchableList
-                repos={repos}
-                username={this.props.username}
-              />
+              <div>
+                <SearchBar
+                  onUserInput={this.handleUserInput}
+                />
+                <RepoList
+                  username={this.props.username}
+                  repos={this.getFilteredRepo()}
+                />
+              </div>
             : <NoResult />
         }
       </div>
@@ -61,109 +151,15 @@ class DataFetchList extends Component {
   }
 }
 
-class SearchableList extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      searchKeyword: ''
-    };
-
-    this.handleUserInput = this.handleUserInput.bind(this);
-    this.getFilteredRepo = this.getFilteredRepo.bind(this);
-  }
-
-  handleUserInput(value) {
-    this.setState({
-      searchKeyword: value
-    });
-  }
-
-  getFilteredRepo() {
-    const { repos } = this.props;
-    return repos.filter((repo) => repo.name.includes(this.state.searchKeyword));
-  }
-
-  render() {
-    return (
-      <div>
-        <SearchBar
-          onUserInput={this.handleUserInput}
-        />
-        <RepoList
-          username={this.props.username}
-          repos={this.getFilteredRepo()}
-        />
-      </div>
-    )
-  }
+DataFetchList.propTypes = {
+  fetch: PropTypes.func,
+  username: PropTypes.string.isRequired,
 }
 
-class SearchBar extends Component {
-  constructor(props) {
-    super(props);
-
-    this.inputSearch = this.inputSearch.bind(this);
-  }
-
-  static PropTypes = {
-    onUserInput: PropTypes.func.isRequired
-  }
-
-  inputSearch(evt) {
-    const val = evt.target.value;
-
-    this.props.onUserInput(val);
-  }
-
-  render() {
-    return (
-      <div>
-        <input
-          name="searchbar"
-          type="text"
-          placeholder="Search..."
-          onChange={this.inputSearch}
-        />
-      </div>
-    );
-  }
-}
-
-function NoResult() {
-  return (
-    <div>
-      "no result"
-    </div>
-  );
-}
-
-class RepoList extends Component {
-  static PropTypes = {
-    username: PropTypes.string.isRequired,
-    repos: PropTypes.array.isRequired
-  }
-
-  render() {
-    const { username, repos } = this.props;
-
-    return (
-      <div>
-        <h1>Repos of {username}:</h1>
-        <ul className="repo-list">{renderDataToList(repos)}</ul>
-      </div>
-    )
-  }
-}
-
-function renderDataToList(data) {
-  let result = [];
-  if (data && data.length > 0) {
-    result = data.map((repo) => <li key={repo.id}>{repo.name}</li>);
-  }
-
-  return result;
+DataFetchList.defaultProps = {
+  fetch: axios.get,
+  username: 'Williammer'
 }
 
 
-export default DataFetchList
+export default DataFetchList;
