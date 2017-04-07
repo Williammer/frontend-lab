9c1f6fb05f1f740b654c5fe1c980c7a2f8336f81
Williammer <supervegeter@gmail.com>
2017-04-07 08:10:14 +0800

    added thunk in middleware

--- redux/src/store/configureStore.dev.js
+++ redux/src/store/configureStore.dev.js
@@ -1,36 +1,33 @@
-import { createStore,/* applyMiddleware,*/ compose } from 'redux'
-// import thunk from 'redux-thunk'
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 // import api from '../middleware/api'
-import rootReducer from '../reducers'
+import rootReducer from '../reducers';
 
-const composeEnhancers =
-  typeof window === 'object' &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-    // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-  }) : compose;
+const composeEnhancers = typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(
+      {
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      }
+    )
+  : compose;
 
-const enhancer = composeEnhancers(
-  // applyMiddleware(...middleware),
-);
+const middleware = [thunk];
 
+const enhancer = composeEnhancers(applyMiddleware(...middleware));
 
 const configureStore = preloadedState => {
-  const store = createStore(
-    rootReducer,
-    preloadedState,
-    enhancer
-  );
+  const store = createStore(rootReducer, preloadedState, enhancer);
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
     module.hot.accept('../reducers', () => {
-      const nextRootReducer = require('../reducers').default
-      store.replaceReducer(nextRootReducer)
-    })
+      const nextRootReducer = require('../reducers').default;
+      store.replaceReducer(nextRootReducer);
+    });
   }
 
-  return store
-}
+  return store;
+};
 
-export default configureStore
+export default configureStore;
