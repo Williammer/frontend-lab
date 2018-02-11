9fc709697d61a506316a1945f7283e6cc88f63a3
William He <supervegeter@gmail.com>
2018-02-11 23:08:08 +0800

    added prettier config; used on index.js

--- redux/src/index.js
+++ redux/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import createHistory from "history/createBrowserHistory";
-import { ConnectedRouter } from "react-router-redux";
+import createHistory from 'history/createBrowserHistory';
+import { ConnectedRouter } from 'react-router-redux';
 import { defaultState } from './store/defaultState';
 import configureStore from './store/configureStore';
 import App from './App';
-import './index.css'
-
+import './index.css';
 
 const store = configureStore(defaultState);
 const history = createHistory();
@@ -18,5 +17,5 @@ ReactDOM.render(
       <App />
     </ConnectedRouter>
   </Provider>,
-  document.getElementById('root')
-)
+  document.getElementById('root'),
+);
