9a2cb8951aad2ef49fa5c3b9f23b44c5baeb9279
Williammer <supervegeter@gmail.com>
2018-03-25 23:03:47 +0800

    Updated rxjs experiment.

--- rxjs/index.js
+++ rxjs/index.js
@@ -3,11 +3,9 @@
  * 	it's an higher-order structure with powerful handling for various kind of events,
  * 	such as click, ajax, timeout, callback, promise, etc.
  */
-var Rx = require('rxjs/Rx');
 
-const obsArr = Rx.Observable.of(1,2,3);
-
-// obsArr.forEach(console.log.bind(console))
-obsArr
-.filter(value => value > 1)
-.subscribe(value => console.log('observed item: '+value));
+const startBtn = document.getElementById('startBtn');
+const console = document.getElementById('console');
+const dblClick = Rx.Observable.fromEvent(startBtn, "dblclick").subscribe(() => {
+    console.textContent = "ddddouble clicked! ";
+});
