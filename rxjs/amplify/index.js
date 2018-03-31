e8cd4addedbe45ee5b031d4ef1ed057070cbcde6
Williammer <supervegeter@gmail.com>
2018-03-31 09:25:27 +0800

    Updated amplify sample; cleanup

--- rxjs/amplify/index.js
+++ rxjs/amplify/index.js
@@ -1,11 +1,33 @@
-/**
- * Rx -> Observer pattern + Iterator + functionalProgramming
- * 	it's an higher-order structure with powerful handling for various kind of events,
- * 	such as click, ajax, timeout, callback, promise, etc.
- */
+const { Observable } = Rx;
 
-const startBtn = document.getElementById('startBtn');
-const console = document.getElementById('console');
-const dblClick = Rx.Observable.fromEvent(startBtn, "dblclick").subscribe(() => {
-    console.textContent = "ddddouble clicked! ";
-});
+Observable.prototype.amplifier = function amplifier(power) {
+  const input = this;
+  return Observable.create(function subscribe(observer) {
+    input.subscribe(
+      ({ width, height }) => {
+        observer.next({
+          width: power * width,
+          height: power * height
+        });
+      },
+      e => observer.error("output err:", e),
+      () => console.log("output complete")
+    );
+  });
+};
+
+const circle = document.getElementById("circle");
+const source = Observable.fromEvent(circle, "click")
+  .map(({ target: { clientWidth, clientHeight } }) => {
+    return { width: clientWidth, height: clientHeight };
+  })
+  .delay(250);
+
+source.amplifier(4).subscribe(
+  ({ width, height }) => {
+    circle.style.width = width + "px";
+    circle.style.height = height + "px";
+  },
+  e => console.log(`error: ${e}`),
+  () => console.log("complete")
+);
