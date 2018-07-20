b0a74d77999d2e6fec16f381bc539c5cd1e79d60
Williammer <supervegeter@gmail.com>
2018-07-20 22:35:43 +0800

    Added tabs and Animation.

--- react/react/src/components/Collapsible.jsx
+++ react/react/src/components/Collapsible.jsx
@@ -1,81 +1,35 @@
-import React, { Component, PureComponent } from "react";
+import React, { Component } from "react";
 import styled from "styled-components";
+import posed from "react-pose";
 
 const collapsibleItems = [
   {
-    title: "🐴",
-    contents: (
-      <div>
-        Horses can sleep both lying down and standing up. Domestic horses have a
-        lifespan of around 25 years. A 19th century horse named 'Old Billy' is
-        said to have lived 62 years.
-      </div>
-    )
+    title: "Kiwifruit 🥝",
+    contents: `
+    The kiwifruit, native to northern China, first arrived in New Zealand at the turn of the 20th
+    century; it was then known as the Chinese Gooseberry.
+    Consuming fruits and vegetables of all kinds is associated with a reduced risk of heart disease,
+    diabetes, cancer, and other conditions.
+    `
   },
   {
-    title: "🦏",
-    contents: (
-      <div>
-        Rhino skin maybe thick but it can be quite sensitive to sunburns and
-        insect bites which is why they like wallow so much – when the mud dries
-        it acts as protection from the sunburns and insects.
-      </div>
-    )
+    title: "Avocado 🥑",
+    contents: `
+    The avocado (Persea americana) is a tree, long thought to have originated in South Central Mexico,
+    classified as a member of the flowering plant family Lauraceae. The fruit of the plant, also
+    called an avocado (or avocado pear or alligator pear), is botanically a large berry containing
+    a single large seed known as a "pit" or a "stone".
+    `
   },
   {
-    title: "🦄",
-    contents: (
-      <div>
-        If you’re looking to hunt a unicorn, but don’t know where to begin, try
-        Lake Superior State University in Sault Ste. Marie, Michigan. Since
-        1971, the university has issued permits to unicorn questers.
-      </div>
-    )
+    title: "Pineapple 🍍",
+    contents: `
+    The pineapple (Ananas comosus) is a tropical plant with an edible multiple fruit consisting of
+    coalesced berries, also called pineapples, and the most economically significant plant in the family
+    Bromeliaceae.
+    `
   }
 ];
-
-// TODOs
-// 2. handles Tab
-// 3. handle animation
-
-class CollapsibleButton extends PureComponent {
-  render() {
-    const { children, onItemClicked } = this.props;
-    return <button onClick={onItemClicked}>{children}</button>;
-  }
-}
-
-class Collapsible extends Component {
-  state = {
-    activeKeys: new Set()
-  };
-
-  onItemClicked(index) {
-    this.setState((prev, { stateReducer = normalCollapsibleReducer }) =>
-      stateReducer(prev, { index })
-    );
-  }
-  render() {
-    const { direction = "down" } = this.props;
-    const opens = this.state.activeKeys;
-
-    return (
-      <div>
-        {collapsibleItems.map(({ title, contents }, index) => (
-          <div key={index}>
-            {opens.has(index) && direction === "up" ? contents : null}
-            <CollapsibleButton
-              onItemClicked={this.onItemClicked.bind(this, index)}
-            >
-              {title}
-            </CollapsibleButton>
-            {opens.has(index) && direction === "down" ? contents : null}
-          </div>
-        ))}
-      </div>
-    );
-  }
-}
 
 function normalCollapsibleReducer(state, { index }) {
   const { activeKeys } = state;
@@ -98,8 +52,175 @@ function singleOpenedReducer(state, source) {
   return normalCollapsibleReducer(state, source);
 }
 
-function CollapsibleExample(props) {
-  return <Collapsible {...props} />;
+function ensureOneOpenedReducer(state, source) {
+  const { activeKeys } = state;
+  const { index } = source;
+
+  if (activeKeys.size > 0) {
+    return { ...state, activeKeys: new Set([index]) };
+  }
+
+  return { ...state, activeKeys: new Set([0]) };
+}
+
+const CollapsibleButton = styled.button`
+  background: ${props => (props.isActive ? "palevioletred" : "white")};
+  cursor: pointer;
+`;
+
+const CollapsibleContent = styled.p`
+  margin: 2px 0;
+`;
+
+const AnimatedCollapsibleContent = posed.div({
+  active: { opacity: 1, top: 30 },
+  inactive: { opacity: 0, top: 100 }
+});
+
+function CollapsibleItem({ item, index, isActive, direction, onItemClicked }) {
+  return (
+    <div>
+      {isActive && direction === "up" ? (
+        <CollapsibleContent>{item.contents}</CollapsibleContent>
+      ) : null}
+      <CollapsibleButton
+        isActive={isActive}
+        onClick={() => onItemClicked(index)}
+      >
+        {item.title}
+      </CollapsibleButton>
+      {isActive && direction === "down" ? (
+        <CollapsibleContent>{item.contents}</CollapsibleContent>
+      ) : null}
+    </div>
+  );
+}
+
+class Collapsible extends Component {
+  static defaultProps = {
+    stateReducer: normalCollapsibleReducer,
+    onStateChange: () => {}
+  };
+
+  state = {
+    activeKeys: new Set(this.props.activeKeys)
+  };
+
+  onItemClicked(index) {
+    this.setState(
+      (prev, { stateReducer }) => stateReducer(prev, { index }),
+      this.props.onStateChanged
+    );
+  }
+  render() {
+    const { direction = "down", render } = this.props;
+    const activeKeys = this.state.activeKeys;
+
+    return render({
+      activeKeys,
+      direction,
+      onItemClicked: this.onItemClicked.bind(this)
+    });
+  }
+}
+
+function Accordion(props) {
+  return (
+    <Collapsible
+      {...props}
+      render={({ activeKeys, direction, onItemClicked }) => (
+        <div>
+          {collapsibleItems.map((item, index) => (
+            <CollapsibleItem
+              key={index}
+              item={item}
+              index={index}
+              direction={direction}
+              isActive={activeKeys.has(index)}
+              onItemClicked={onItemClicked}
+            />
+          ))}
+        </div>
+      )}
+    />
+  );
+}
+
+function Tabs(props) {
+  return (
+    <Collapsible
+      activeKeys={[0]}
+      {...props}
+      stateReducer={ensureOneOpenedReducer}
+      render={({ activeKeys, direction, onItemClicked }) => {
+        const tabs = collapsibleItems.map((item, index) => (
+          <CollapsibleButton
+            key={index}
+            isActive={activeKeys.has(index)}
+            onClick={() => onItemClicked(index)}
+          >
+            {item.title}
+          </CollapsibleButton>
+        ));
+
+        const contents = collapsibleItems.map(
+          (item, index) =>
+            activeKeys.has(index) ? (
+              <CollapsibleContent key={index}>
+                {item.contents}
+              </CollapsibleContent>
+            ) : null
+        );
+
+        return (
+          <div>
+            {direction === "up" ? contents : null}
+            {tabs}
+            {direction === "down" ? contents : null}
+          </div>
+        );
+      }}
+    />
+  );
+}
+
+function AnimatedTabs(props) {
+  return (
+    <Collapsible
+      activeKeys={[0]}
+      {...props}
+      stateReducer={ensureOneOpenedReducer}
+      render={({ activeKeys, direction, onItemClicked }) => {
+        const tabs = collapsibleItems.map((item, index) => (
+          <CollapsibleButton
+            key={index}
+            isActive={activeKeys.has(index)}
+            onClick={() => onItemClicked(index)}
+          >
+            {item.title}
+          </CollapsibleButton>
+        ));
+
+        const contents = collapsibleItems.map((item, index) => (
+          <AnimatedCollapsibleContent
+            key={index}
+            pose={activeKeys.has(index) ? "active" : "inactive"}
+            style={{ position: "absolute" }}
+          >
+            {item.contents}
+          </AnimatedCollapsibleContent>
+        ));
+
+        return (
+          <div style={{ position: "relative" }}>
+            {direction === "up" ? contents : null}
+            {tabs}
+            {direction === "down" ? contents : null}
+          </div>
+        );
+      }}
+    />
+  );
 }
 
 const DemoContainer = styled.div`
@@ -114,24 +235,42 @@ const DemoItem = styled.li`
 export default function Demo() {
   const demos = [
     {
-      demoTitle: "Direction: up",
+      demoTitle: "Accordion: up",
+      Component: Accordion,
       direction: "up"
     },
     {
-      demoTitle: "Direction: down",
+      demoTitle: "Accordion: down",
+      Component: Accordion,
       direction: "down"
     },
     {
-      demoTitle: "Single Opened",
+      demoTitle: "Accordion: Single Opened",
+      Component: Accordion,
       stateReducer: singleOpenedReducer
+    },
+    {
+      demoTitle: "Tab: down",
+      Component: Tabs,
+      direction: "down"
+    },
+    {
+      demoTitle: "Tab: up",
+      Component: Tabs,
+      direction: "up"
+    },
+    {
+      demoTitle: "AnimatedTabs: down",
+      Component: AnimatedTabs,
+      direction: "down"
     }
   ];
   return (
     <DemoContainer>
-      {demos.map(({ demoTitle, ...props }, index) => (
+      {demos.map(({ demoTitle, Component, ...props }, index) => (
         <DemoItem key={index}>
           <h3>{demoTitle}</h3>
-          <CollapsibleExample {...props} />
+          <Component {...props} />
         </DemoItem>
       ))}
     </DemoContainer>
