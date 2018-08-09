0a8aa32262c0d1070c62efd7b6780a5a3ea189ef
Williammer <supervegeter@gmail.com>
2018-08-09 16:39:46 +0800

    cleanup old pkgs

--- react/redux/src/epics/fetchUserRepos.js
+++ react/redux/src/epics/fetchUserRepos.js
@@ -1,27 +1,8 @@
-import { normalize, schema } from 'normalizr';
-import { camelizeKeys } from 'humps';
 import { ofType } from 'redux-observable';
 import { switchMap, map, catchError } from 'rxjs/operators';
 import { of, from } from 'rxjs';
 import { FETCH_REPOS } from '../constants';
 import { updateRepos, fetchReposFailure } from '../actions';
-
-const userSchema = new schema.Entity(
-  'users',
-  {},
-  {
-    idAttribute: user => user.login.toLowerCase(),
-  },
-);
-const repoSchema = new schema.Entity(
-  'repos',
-  {
-    owner: userSchema,
-  },
-  {
-    idAttribute: repo => repo.name.toLowerCase(),
-  },
-);
 
 async function fetchUserRepos(fetch, username) {
   const endpoint = `https://api.github.com/users/${username}/repos`;
