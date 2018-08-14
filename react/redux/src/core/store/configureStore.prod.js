468e20242335f1c8368543aa0ffd22dbaed04b44
Williammer <supervegeter@gmail.com>
2018-08-14 17:48:59 +0800

    fix

--- react/redux/src/core/store/configureStore.prod.js
+++ react/redux/src/core/store/configureStore.prod.js
@@ -1,13 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
-import { createBrowserHistory } from 'history';
-import { connectRouter, routerMiddleware } from 'connected-react-router';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { createEpicMiddleware } from 'redux-observable';
 import { get } from 'axios';
 import rootReducer from '../rootReducer';
 import rootEpic from '../rootEpic';
 
-const history = createBrowserHistory();
 const epics = createEpicMiddleware({
   dependencies: {
     fetch: get,
