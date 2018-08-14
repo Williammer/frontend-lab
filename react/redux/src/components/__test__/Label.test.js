23113b03965e9c42b09a7e918d8962082db4f31d
Williammer <supervegeter@gmail.com>
2018-08-14 16:40:58 +0800

    rm unused

--- react/redux/src/components/__test__/Label.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Label from '../Label';
-
-it('renders Label without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Label />, div);
-});
